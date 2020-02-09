f422752e787cf885ec7b4467e5d2d93143cdac84
Michael De La Rosa <mdelarosa86@gmail.com>
2020-02-09 20:44:28 +0000

    Automatically backed up by Learn

---
+++ loops.rb
@@ -0,0 +1,58 @@
+# count = 0
+# while count < 3 do 
+#   puts "I am the counter: #{count}!"
+#   count += 1
+# end
+
+# magic_exit_number = 7
+# count = 0
+# while count < 10 do
+#   break if count == magic_exit_number
+#   puts "I am the #{count}!"
+#   count += 1
+# end
+
+# magic_exit_number = 7
+# count = 1
+# while count < 10 && count != magic_exit_number
+# do
+#   puts "I am the count: #{count}!"
+#   count += 1
+# end
+
+# n = 2
+# count = 0
+# while count <= n do
+#   puts "I ran."
+#   count += 1
+# end
+
+# #Instead of the above we would use this:
+# 3.times do 
+#   puts "I ran."
+# end
+
+# #Inifinite loop with a break using loop:
+# count = 0
+# n = 3
+# loop do
+#   break if count >= n
+#   puts "I ran."
+#   count += 1
+# end
+
+# #While version of the loop above: Use the above instead.
+# count = 0
+# n = 3
+# while true do
+#   break if count >= n
+#   puts "I ran."
+#   count += 1
+# end
+
+counter = 0
+until counter == 10
+  puts "The current number is less than 10."
+  counter += 1
+end
+
