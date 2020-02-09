96786108ea04de38bbd961e6de610d676dab118d
Michael De La Rosa <mdelarosa86@gmail.com>
2020-02-09 20:44:41 +0000

    Automatically backed up by Learn

--- loops.rb
+++ loops.rb
@@ -50,9 +50,9 @@
 #   count += 1
 # end
 
-counter = 0
-until counter == 10
-  puts "The current number is less than 10."
-  counter += 1
-end
+# counter = 0
+# until counter == 10
+#   puts "The current number is less than 10."
+#   counter += 1
+# end
 
