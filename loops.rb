126591e99ddae38044b6b39db632da9400db169d
Michael De La Rosa <mdelarosa86@gmail.com>
2020-02-09 20:49:30 +0000

    Automatically backed up by Learn

--- loops.rb
+++ loops.rb
@@ -69,7 +69,15 @@
 array = [1,2,3,4,5]
 length = array.length
 
+length.times do | index |
+  puts array[index]
+end
+
+# Or if we don't want to use the outside length variable, you can chain like so:
+
 array.length.times do | index |
   puts array[index]
 end
+
+
 
